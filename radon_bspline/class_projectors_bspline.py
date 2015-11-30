d4d41861a0bc7a3e37ae68454bbe420997455328
arcaduf <arcus@gmail.com>
2015-11-30 09:54:58 +0100

    FIX: fbp with cubic b-spline works with lut for radon degree 0

--- radon_bspline/class_projectors_bspline.py
+++ radon_bspline/class_projectors_bspline.py
@@ -39,27 +39,33 @@ class projectors:
 
     ##  Init class projectors
     def __init__( self , npix , angles , ctr=0.0 , bspline_degree = 3 , proj_support_y=4 ,
-                  nsamples_y=2048 , radon_degree=1 , filt='ramp' , plot=False ):
+                  nsamples_y=2048 , radon_degree=0 , filt='ramp' , plot=False ):
     
         ##  Compute regridding look-up-table and deapodizer
         self.plot = plot
         nang = len( angles )
         angles = np.arange( nang )
         angles = ( angles * 180.0 )/myfloat( nang )         
-        lut = bfun.init_lut_bspline( nsamples_y , angles , bspline_degree ,
-                                     radon_degree , proj_support_y )
+        lut = bfun.init_lut_bspline( nsamples_y , angles , 
+                                     bspline_degree ,
+                                     0 ,
+                                     #radon_degree , 
+                                     proj_support_y )
         
        
         if plot is True:
             dis.plot( lut , 'Look-up-table' )          
 
 
+
         ##  Assign parameters
-        self.lut          = lut.astype( myfloat )
-        self.angles       = angles.astype( myfloat )
-        self.param_spline = np.array( [ lut.shape[1] , proj_support_y ] , dtype=myfloat )
-        self.filt         = filt
-        self.radon_degree = radon_degree
+        self.lut            = lut.astype( myfloat )
+        self.angles         = angles.astype( myfloat )
+        self.nang           = nang
+        self.bspline_degree = bspline_degree
+        self.param_spline   = np.array( [ lut.shape[1] , proj_support_y ] , dtype=myfloat )
+        self.filt           = filt
+        self.radon_degree   = radon_degree
 
 
 
@@ -78,8 +84,35 @@ class projectors:
 
     ##  Filtered backprojection
     def fbp( self , x ):
-        x[:,:] = fil.filter_fft( x , self.filt , self.radon_degree )
+        ##  Option DPC
+        if self.radon_degree == 0:
+            dpc = False
+        else:
+            dpc = True
+
+        
+        ##  Filtering projection
+        x[:] = fil.filter_proj( x , ftype=self.filt , dpc=dpc )
         if self.plot is True:
             dis.plot( x , 'Filtered sinogram' )
-        return gr.backproj( x.astype( myfloat ) , self.angles , self.lut , self.param_spline ) 
+    
+        
+        ##  Backprojection
+        reco = gr.backproj( x.astype( myfloat ) , self.angles , self.lut , self.param_spline )
+
+
+        ##  Normalization
+        if dpc is True:
+            reco *= np.pi / ( 1.0 * self.nang )
+        else:
+            reco *= np.pi / ( 2.0 * self.nang )         
+
+        return reco
+
+
+
+    ##  Conversion to pixel basis
+    def conv_to_pix( self , x ):
+        print(  self.bspline_degree )
+        return bfun.convert_from_bspline_to_pixel_basis( x , self.bspline_degree )     
 
